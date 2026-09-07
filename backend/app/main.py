fb75e8c1bfd50cb90a1017688aed3cf489b7dca5
Diego <diego120689@gmail.com>
2026-09-07 17:30:53 -0300

    feat: add production env and update CORS for Vercel deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,11 @@ app = FastAPI(
 # Allows the React frontend to access the API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://spendwise.vercel.app",
+        "ttps://spendwise-pi.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
